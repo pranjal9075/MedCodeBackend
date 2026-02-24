73158828eade9e00fcec8be7a5de4ab2d43cbade
Pranjal Supekar <pranjalsupekar91@gmail.com>
2026-02-24 14:50:56 +0530

    add ssl problem

--- config/db.js
+++ config/db.js
@@ -5,9 +5,9 @@ const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,   // 🔥 IMPORTANT
+  port: process.env.DB_PORT,
   ssl: {
-    rejectUnauthorized: false  // 🔥 Aiven requires SSL
+    rejectUnauthorized: false
   },
   waitForConnections: true,
   connectionLimit: 10,
